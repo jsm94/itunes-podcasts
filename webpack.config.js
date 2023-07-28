c93c21544936b9290beedb561afb45ec6952d4ab
Juan Antonio Seco Merchán <juan.seco@atmira.com>
2023-07-28 12:21:41 +0200

    build: add loaders for css and pluging for html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const src = path.resolve(__dirname, "src");
+
+const ruleForTSX = {
+  test: /\.(jsx?|tsx?)$/,
+  include: src,
+  loader: "babel-loader",
+  options: {
+    presets: [
+      "@babel/preset-env",
+      "@babel/preset-typescript",
+      ["@babel/preset-react", { runtime: "automatic" }],
+    ],
+  },
+};
+
+const ruleForCSS = {
+  test: /\.css$/,
+  include: src,
+  use: ["style-loader", "css-loader"],
+};
+
+const rules = [ruleForTSX, ruleForCSS];
 
 module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(src, "index.html"),
+    }),
+  ],
   module: {
-    rules: [
-      {
-        test: /\.(jsx?|tsx?)$/,
-        include: src,
-        loader: "babel-loader",
-        options: {
-          presets: [
-            "@babel/preset-env",
-            "@babel/preset-typescript",
-            ["@babel/preset-react", { runtime: "automatic" }],
-          ],
-        },
-      },
-    ],
+    rules,
   },
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
 };
