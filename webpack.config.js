abd61389e4612dee97480249dacb62e70b1fbc20
Juan Antonio Seco Merchán <juan.seco@atmira.com>
2023-07-28 10:46:19 +0200

    build: add typescript and babel loaders for webpack and configurations files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,27 @@
 const path = require("path");
+const src = path.resolve(__dirname, "src");
 
 module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?|tsx?)$/,
+        include: src,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env",
+            "@babel/preset-typescript",
+            ["@babel/preset-react", { runtime: "automatic" }],
+          ],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
 };
