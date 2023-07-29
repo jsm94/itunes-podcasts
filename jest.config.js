d9fa676ec51ee14c6945e471c0ed2afe84fc5bbe
Juan Antonio Seco Merchán <juan.seco@atmira.com>
2023-07-29 11:39:24 +0200

    fix: parse  correctly css files on testing

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ const config = {
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.tsx?$": "babel-jest",
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
   },
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
 };
