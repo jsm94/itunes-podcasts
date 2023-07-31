e0e47430c5cdf4e8597d94763cb78c216cc8c312
Juan Antonio Seco Merchán <juan.seco@atmira.com>
2023-07-31 14:18:35 +0200

    feat: add cache feature with a custom localStorage hook

--- src/hooks/podcasts/usePodcasts.tsx
+++ src/hooks/podcasts/usePodcasts.tsx
@@ -2,13 +2,23 @@ import { useState } from "react";
 import { PodcastService } from "../../modules/podcasts/application/PodcastService";
 import { Podcast } from "../../modules/podcasts/domain/Podcast";
 import { ApiPodcastService } from "../../modules/podcasts/infra/ApiPodcastService";
+import { useLocalStorage } from "../localStorage/useLocalStorage";
+
+const KEY_PODCASTS = "podcasts";
 
 export const usePodcasts = () => {
   const [podcasts, setPodcasts] = useState<Podcast[]>([]);
+  const { setItem, getItem } = useLocalStorage<Podcast[]>();
 
   const getPodcasts = async () => {
+    const cachedPodcasts = getItem(KEY_PODCASTS);
+    if (cachedPodcasts) {
+      setPodcasts(cachedPodcasts);
+      return;
+    }
     const podcastApi = new PodcastService(new ApiPodcastService());
     const response = await podcastApi.getMostPopularPodcasts(100);
+    setItem(KEY_PODCASTS, response);
     setPodcasts(response);
   };
 
